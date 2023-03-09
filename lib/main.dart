1379dda8cb17b3c1950819e62a630c14a06606eb
Guuisr07 <guisantana0204@gmail.com>
2023-03-09 01:48:37 -0300

    change main.yml

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_firebase_hosting/firebase_options.dart';
 
 import 'myapp.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+void main() {
   runApp(const MyApp());
 }
